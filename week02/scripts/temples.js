69e43eae269d90368e5eec3e9d98ac8428dda342
Ezequiel Rindello <rindelloezequiel@gmail.com>
2024-09-25 22:24:50 -0300

    javascrit and gallery created

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,3 +1,4 @@
+// dates
 let lastModifiedDate = document.lastModified;
 document.getElementById("lastModified").textContent = lastModifiedDate;
 
@@ -7,10 +8,44 @@ document.addEventListener("DOMContentLoaded", function () {
   yearElement.textContent = currentYear;
 });
 
-// menu
+// movile menu
 const mobileMenu = document.getElementById("mobile-menu");
 const navList = document.querySelector(".nav-list");
 
 mobileMenu.addEventListener("click", () => {
   navList.classList.toggle("active");
 });
+
+// json galery
+async function fetchData() {
+  try {
+    const data = await fetch("./temples.json");
+    const response = await data.json();
+    return response.temples;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function createGallery() {
+  const galery = document.getElementById("temples");
+  const templesData = await fetchData();
+
+  templesData.forEach((temple) => {
+    const figureElement = document.createElement("figure");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = temple.url;
+    imgElement.alt = `image ${temple.name}`;
+
+    const captionElement = document.createElement("figcaption");
+    captionElement.textContent = temple.name; 
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(captionElement);
+
+    galery.appendChild(figureElement);
+  });
+}
+
+createGallery();
