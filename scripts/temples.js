6a75b659701553c1a46327b149b4e7cf20791315
Ezequiel Rindello <rindelloezequiel@gmail.com>
2024-10-07 21:42:55 -0300

    week04

--- scripts/temples.js
+++ scripts/temples.js
@@ -35,17 +35,175 @@ async function createGallery() {
     const figureElement = document.createElement("figure");
 
     const imgElement = document.createElement("img");
-    imgElement.src = temple.url;
-    imgElement.alt = `image ${temple.name}`;
+    imgElement.src = temple.imageUrl;
+    imgElement.alt = `image ${temple.templeName}`;
+    imgElement.loading = "lazy";
 
     const captionElement = document.createElement("figcaption");
-    captionElement.textContent = temple.name; 
+    captionElement.classList.add("main-caption");
+    captionElement.textContent = temple.templeName;
+
+    const locationElement = document.createElement("figcaption");
+    locationElement.textContent = `Location: ${temple.location}`;
+
+    const dedicatedElement = document.createElement("figcaption");
+    dedicatedElement.textContent = `Dedicated: ${temple.dedicated}`;
+
+    const sizeElement = document.createElement("figcaption");
+    sizeElement.textContent = `Size: ${temple.area}`;
 
     figureElement.appendChild(imgElement);
     figureElement.appendChild(captionElement);
+    figureElement.appendChild(locationElement);
+    figureElement.appendChild(dedicatedElement);
+    figureElement.appendChild(sizeElement);
 
     galery.appendChild(figureElement);
   });
 }
 
 createGallery();
+
+// week03
+async function getOldTemples() {
+  const galery = document.getElementById("temples");
+  galery.innerHTML = "";
+  galery.textContent = "";
+  const templesData = await fetchData();
+  const oldTemples = templesData.filter((temple) => temple.date < 2000);
+
+  oldTemples.forEach((temple) => {
+    const figureElement = document.createElement("figure");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = temple.imageUrl;
+    imgElement.alt = `image ${temple.templeName}`;
+    imgElement.loading = "lazy";
+
+    const captionElement = document.createElement("figcaption");
+    captionElement.classList.add("main-caption");
+    captionElement.textContent = temple.templeName;
+
+    const locationElement = document.createElement("figcaption");
+    locationElement.textContent = `Location: ${temple.location}`;
+
+    const dedicatedElement = document.createElement("figcaption");
+    dedicatedElement.textContent = `Dedicated: ${temple.dedicated}`;
+
+    const sizeElement = document.createElement("figcaption");
+    sizeElement.textContent = `Size: ${temple.area}`;
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(captionElement);
+    figureElement.appendChild(locationElement);
+    figureElement.appendChild(dedicatedElement);
+    figureElement.appendChild(sizeElement);
+
+    galery.appendChild(figureElement);
+  });
+}
+
+async function getNewTemples() {
+  const galery = document.getElementById("temples");
+  galery.innerHTML = "";
+  galery.textContent = "";
+  const templesData = await fetchData();
+  const newTemples = templesData.filter((temple) => temple.date > 2000);
+
+  newTemples.forEach((temple) => {
+    const figureElement = document.createElement("figure");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = temple.imageUrl;
+    imgElement.alt = `image ${temple.templeName}`;
+    imgElement.loading = "lazy";
+
+    const captionElement = document.createElement("figcaption");
+    captionElement.classList.add("main-caption");
+    captionElement.textContent = temple.templeName;
+
+    const locationElement = document.createElement("figcaption");
+    locationElement.textContent = `Location: ${temple.location}`;
+
+    const dedicatedElement = document.createElement("figcaption");
+    dedicatedElement.textContent = `Dedicated: ${temple.dedicated}`;
+
+    const sizeElement = document.createElement("figcaption");
+    sizeElement.textContent = `Size: ${temple.area}`;
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(captionElement);
+    figureElement.appendChild(locationElement);
+    figureElement.appendChild(dedicatedElement);
+    figureElement.appendChild(sizeElement);
+
+    galery.appendChild(figureElement);
+  });
+}
+
+async function getLargeTemples() {
+  const galery = document.getElementById("temples");
+  galery.innerHTML = "";
+  galery.textContent = "";
+  const templesData = await fetchData();
+  const largeTemples = templesData.filter((temple) => temple.area > 20000);
+
+  largeTemples.forEach((temple) => {
+    const figureElement = document.createElement("figure");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = temple.imageUrl;
+    imgElement.alt = `image ${temple.templeName}`;
+    imgElement.loading = "lazy";
+
+    const captionElement = document.createElement("figcaption");
+    captionElement.classList.add("main-caption");
+    captionElement.textContent = temple.templeName;
+
+    const locationElement = document.createElement("figcaption");
+    locationElement.textContent = `Location: ${temple.location}`;
+
+    const dedicatedElement = document.createElement("figcaption");
+    dedicatedElement.textContent = `Dedicated: ${temple.dedicated}`;
+
+    const sizeElement = document.createElement("figcaption");
+    sizeElement.textContent = `Size: ${temple.area}`;
+
+    figureElement.appendChild(imgElement);
+    figureElement.appendChild(captionElement);
+    figureElement.appendChild(locationElement);
+    figureElement.appendChild(dedicatedElement);
+    figureElement.appendChild(sizeElement);
+
+    galery.appendChild(figureElement);
+  });
+}
+
+const oldT = document.getElementById("old");
+const largeT = document.getElementById("large");
+const newT = document.getElementById("new");
+const homeT = document.getElementById("home");
+const currentSelection = document.getElementById("current-selection");
+
+oldT.addEventListener("click", () => {
+  currentSelection.textContent = "Old Temples";
+  getOldTemples();
+});
+
+newT.addEventListener("click", () => {
+  currentSelection.textContent = "New Temples";
+  getNewTemples();
+});
+
+largeT.addEventListener("click", () => {
+  currentSelection.textContent = "Large Temples";
+  getLargeTemples();
+});
+
+homeT.addEventListener("click", () => {
+  currentSelection.textContent = "Home";
+  const galery = document.getElementById("temples");
+  galery.innerHTML = "";
+  galery.textContent = "";
+  createGallery();
+});
